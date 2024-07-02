29ad8ef75b1fb77316f62573e14eace57a60a3bd
Leandro Tercette <tal1wrn@bosch.com>
2024-07-02 09:28:13 -0300

    feat: creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { GameControlsComponent } from './game-controls/game-controls.component';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { CardDisplayComponent } from './card-display/card-display.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    GameControlsComponent,
+    UserInfoComponent,
+    CardDisplayComponent,
+ 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    FormsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/game-controls/game-controls.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-controls',
+  templateUrl: './game-controls.component.html',
+  styleUrl: './game-controls.component.css'
+})
+export class GameControlsComponent {
+
+}
